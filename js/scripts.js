1fec424793326f021da020ddd6f906a7ded6c91d
Chris Swanner and Misha Petrov <chris@>
2018-01-08 15:14:02 -0800

    Add javascript for peekaboo site.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,43 @@
-var toLiters = function(gallons) {
+/*var toLiters = function(gallons) {
   return gallons * 3.785;
 }
-var gallons = parseFloat(prompt("Enter gallon amount:"));
-alert(gallons + " gallons is " + toLiters(gallons) + " liters");
+//var gallons = parseFloat(prompt("Enter gallon amount:"));
+//alert(gallons + " gallons is " + toLiters(gallons) + " liters");
+
+var userInput = prompt("Enter a sentence: ");
+var length = userInput.length;
+
+var capitalizeFirstLast = function (sentence) {
+  return sentence.charAt(0).toUpperCase() + sentence.charAt(length-1).toUpperCase();
+};
+var reverse = function(sentence) {
+  var firstLetter = sentence.charAt(0);
+  var lastLetter = sentence.charAt(1);
+  sentence = lastLetter + firstLetter;
+  return sentence;
+};
+var call = function(sentence) {
+  var result = capitalizeFirstLast(sentence);
+  return userInput + reverse(result);
+};
+var grabIndex = function(sentence) {
+  var index = length / 2;
+  return sentence.charAt(index) + call(sentence);
+};
+var finalReverse = function(sentence) {
+  var length = sentence.length;
+  var new_string = sentence.charAt(length-1) + sentence.charAt(length-2) + sentence.charAt(length-3);
+}
+//alert(capitalizeFirstLast(userInput));
+alert(grabIndex(userInput));
+//capitalizeFirstLast(userInput);
+//alert(userInput);
+*/
+$(document).ready(function() {
+
+  $(".clickable").click(function() {
+    $("#initially_showing").toggle();
+    $("#initially_hidden").toggle();
+  });
+
+});
